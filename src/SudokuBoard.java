17d8ca37cdd7dfceb2c7be1de28c465c2fd8c041
cpleas <cpleas@medallia.com>
2020-05-25 01:18:23 +0100

    Added solver.

--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -2,64 +2,119 @@ package src;
 
 /**
  * Board that represents a Sudoku puzzle. Puzzle may be fully or partially solved.
- * Unsolved squares will have the value 0.
+ * Unsolved squares have the value 0.
  */
 public class SudokuBoard {
-    private int[] board;
+	public int[] board;
+	int[] res;
 
-    public SudokuBoard(int[] board) {
-        if (board == null || board.length != 81) {
-            throw new IllegalArgumentException("Board must be of length 9");
-        }
-        for (int i : board) {
-            if (i < 0 || i > 9) {
-                throw new IllegalArgumentException(String.format("Invalid value in board %d", board[i]));
-            }
-        }
-        this.board = board;
-    }
+	public SudokuBoard(int[] board) {
+		if (board == null || board.length != 81) {
+			throw new IllegalArgumentException("Board must be of length 81");
+		}
+		for (int i : board) {
+			if (i < 0 || i > 9) {
+				throw new IllegalArgumentException(String.format("Invalid value in board %d", board[i]));
+			}
+		}
+		this.board = board;
+	}
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < board.length; i++) {
-            if (i != 0 && i % 9 == 0) {
-                sb.append("\n");
-            } else if (i != 0) {
-                sb.append(" ");
-            }
-            sb.append(board[i]);
-        }
-        return sb.toString();
-    }
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < board.length; i++) {
+			if (i != 0 && i % 9 == 0) {
+				sb.append("\n");
+			} else if (i != 0) {
+				sb.append(" ");
+			}
+			sb.append(board[i]);
+		}
+		return sb.toString();
+	}
 
-    private boolean isValid() {
-        return true;
-    }
+	/**
+	 * Check for errors in rows, columns, and boxes.
+	 * Loops from (0,8) on inner and outer for loops.
+	 * <p>
+	 * "seenIn" arrays act like sets that checks to see if the value
+	 * is already a member. They are reset on each outer loop.
+	 * <p>
+	 * The box variable runs from (0,8) and represents different boxes
+	 * in the puzzle starting at the top right box and then running
+	 * reading direction (right and then down) through all of the boxes.
+	 */
+	public boolean isValid() {
+		// Row and column validation
+		boolean[] seenInRows, seenInCols;
+		int rowVal, colVal;
 
-    /* Utility function to get the value at a certain (row, column) equivalent */
-    private int get(int row, int col){
-        return this.board[col + row * 9];
-    }
+		for (int i = 0; i < 9; i++) {
+			seenInRows = new boolean[]{false, false, false, false, false, false, false, false, false};
+			seenInCols = new boolean[]{false, false, false, false, false, false, false, false, false};
+			for (int j = 0; j < 9; j++) {
+				// Columns and row validation
+				rowVal = this.get(i, j);
+				colVal = this.get(j, i);
+				if (rowVal != 0) {
+					if (seenInRows[rowVal - 1]) return false;
+					else seenInRows[rowVal - 1] = true;
+				}
+				if (colVal != 0) {
+					if (seenInCols[colVal - 1]) return false;
+					else seenInCols[colVal - 1] = true;
+				}
+			}
+		}
 
-    private boolean rowsAreValid() {
-        boolean[] seen;
-        for (int row = 0; row<9; row++) {
-            seen = new boolean[]{false, false, false, false, false, false, false, false, false};
-            for (int col = 0; col < 9; col++) {
-                if (this.get(row, col) == 0) {
+		// Box validation
+		boolean[] seenInBox;
+		int anchorRow, anchorCol, boxVal;
+		for (int box = 0; box < 9; box++) {
+			seenInBox = new boolean[]{false, false, false, false, false, false, false, false, false};
+			anchorCol = (box % 3) * 3;
+			anchorRow = (box / 3) * 3;
+			for (int row = 0; row < 3; row++) {
+				for (int col = 0; col < 3; col++) {
+					boxVal = this.get(anchorRow + row, anchorCol + col);
+					if (boxVal != 0) {
+						if (seenInBox[boxVal - 1]) return false;
+						else seenInBox[boxVal - 1] = true;
+					}
+				}
+			}
+		}
 
-                }
-            }
-        }
-        //fixme
-        return true;
-    }
+		// The position is valid
+		return true;
+	}
 
-    public int[] solve() {
-        if (!this.isValid()) {
-            throw new IllegalStateException("Initial board is invalid");
-        }
-        SudokuBoard holder = new SudokuBoard(this.board);
-        return holder.board;
-    }
+	/* Utility function to get the value at a certain (row, column) equivalent */
+	private int get(int row, int col) {
+		return this.board[col + row * 9];
+	}
+
+	/* Utility function to get the value at a certain (row, column) equivalent */
+	private void set(int row, int col, int val) {
+		this.board[col + row * 9] = val;
+	}
+
+	public boolean solve() {
+		if (!isValid()) return false;
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				// Search for an empty cell
+				if (get(row, col) == 0) {
+					// Try all values
+					for (int val = 1; val <= 9; val++) {
+						set(row, col, val);
+						if (solve()) return true;
+						set(row, col, 0);
+					}
+					return false; // we return false
+				}
+			}
+		}
+		return true;
+	}
 }
--- src/SudokuBoardTest.java
+++ src/SudokuBoardTest.java
@@ -85,7 +85,149 @@ class SudokuBoardTest {
     }
 
     @Test
-    void simpleSolve() {
-        SudokuBoard s = new SudokuBoard(fullValid);
+    void simpleValidation() {
+        SudokuBoard s;
+
+        int[] fullInvalid = {
+                1, 8, 4, 9, 6, 3, 7, 2, 5,
+                5, 6, 2, 7, 4, 8, 3, 1, 9,
+                3, 9, 7, 5, 1, 2, 8, 6, 4,
+                2, 3, 9, 6, 5, 7, 1, 4, 8,
+                7, 5, 6, 1, 8, 4, 2, 9, 3,
+                4, 1, 8, 2, 3, 9, 6, 5, 7,
+                9, 4, 1, 3, 7, 6, 5, 8, 2,
+                6, 2, 3, 8, 9, 5, 4, 7, 7, // note the duplicate 7s
+                8, 7, 5, 4, 2, 1, 9, 3, 6
+        };
+        Assertions.assertFalse(new SudokuBoard(fullInvalid).isValid());
+
+        int[] rowInvalid = {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 7, 7, // note the duplicate 7s
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assertions.assertFalse(new SudokuBoard(rowInvalid).isValid());
+
+        int[] colInvalid = {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 3, 0, 0, 0, 0, 0, 0, 0, // note the duplicate 3s
+                0, 3, 0, 0, 0, 0, 0, 0, 0, // note the duplicate 3s
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assertions.assertFalse(new SudokuBoard(colInvalid).isValid());
+
+        int[] boxInvalid = {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 1, 0, 0, // note the duplicate 1s
+                0, 0, 0, 0, 0, 0, 0, 0, 1, // note the duplicate 1s
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assertions.assertFalse(new SudokuBoard(boxInvalid).isValid());
+
+        Assertions.assertTrue(new SudokuBoard(fullValid).isValid());
     }
+
+    @Test
+    void trivialSolve() {
+        SudokuBoard s = new SudokuBoard(fullValid);
+        Assertions.assertEquals(true, s.solve());
+        Assertions.assertArrayEquals(s.board, fullValid);
+    }
+
+    @Test
+    void easySolve() {
+        int[] easyValid = {
+                1, 8, 4, 9, 6, 3, 7, 2, 5,
+                5, 6, 2, 7, 4, 8, 3, 1, 9,
+                3, 9, 7, 5, 1, 2, 8, 6, 4,
+                2, 3, 9, 6, 5, 7, 1, 4, 8,
+                7, 5, 6, 1, 0, 4, 2, 9, 3, // note the blank
+                4, 1, 8, 2, 3, 9, 6, 5, 7,
+                9, 4, 1, 3, 7, 6, 5, 8, 2,
+                6, 2, 3, 8, 9, 5, 4, 7, 1,
+                8, 7, 5, 4, 2, 1, 9, 3, 6
+        };
+        SudokuBoard s = new SudokuBoard(easyValid);
+        Assertions.assertEquals(true, s.solve());
+        Assertions.assertArrayEquals(s.board, fullValid);
+    }
+
+    @Test
+    void mediumSolve() {
+        int[] mediumValid = {
+                1, 8, 4, 9, 0, 3, 7, 2, 5,
+                5, 6, 2, 7, 0, 8, 3, 1, 9,
+                3, 9, 7, 5, 0, 2, 8, 6, 4,
+                0, 3, 9, 6, 0, 7, 1, 4, 8,
+                0, 0, 6, 1, 0, 4, 2, 9, 3,
+                4, 1, 8, 2, 0, 9, 6, 5, 7,
+                9, 4, 1, 3, 0, 6, 5, 8, 2,
+                6, 2, 3, 8, 0, 5, 4, 7, 1,
+                8, 7, 5, 4, 0, 1, 9, 3, 6
+        };
+        SudokuBoard s = new SudokuBoard(mediumValid);
+        Assertions.assertEquals(true, s.solve());
+        Assertions.assertArrayEquals(s.board, fullValid);
+    }
+
+    @Test
+    void emptySolve() {
+        int[] empty = {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        SudokuBoard s = new SudokuBoard(empty);
+        Assertions.assertEquals(true, s.solve());
+    }
+
+    @Test
+    void hardestSolve() {
+        int[] empty = {
+                8, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 3, 6, 0, 0, 0, 0, 0,
+                0, 7, 0, 0, 9, 0, 2, 0, 0,
+                0, 5, 0, 0, 0, 7, 0, 0, 0,
+                0, 0, 0, 0, 4, 5, 7, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 3, 0,
+                0, 0, 1, 0, 0, 0, 0, 6, 8,
+                0, 0, 8, 5, 0, 0, 0, 1, 0,
+                0, 9, 0, 0, 0, 0, 4, 0, 0
+        };
+        SudokuBoard s = new SudokuBoard(empty);
+        Assertions.assertEquals(true, s.solve());
+        System.out.println(s);
+    }
+
+
+
+
+
+
+
+
+
+
 }
